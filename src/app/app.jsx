42988afc0e7de1a5f3b38011052af766be1e8593
Paweł Kapalla <pkapalla@xesenix.pl>
2018-02-23 23:31:38 +0100

    WPS-09 feat(theme): style color property variable

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -5,12 +5,23 @@ import { hot } from 'react-hot-loader';
 import Timer from './component/timer';
 
 class App extends React.Component {
+	updateColorTheme() {
+		console.log('change', this.colorInput.value);
+		document.documentElement.style.setProperty('--timerColor', this.colorInput.value);
+	}
+
 	render() {
 		return (<div className="box">
 			<strong>REACT is working</strong><br/>
 			<code>src="assets/images/d.png"</code><br/>
 			<img src="assets/images/d.png"/><br/>
 			<Timer></Timer>
+			<h3>Swap theme color via style property variable</h3>
+			<p>This works post sass so no sass functionality may be used over this color variable.</p>
+			<div className="form-group">
+				<label>Color</label>
+				<input className="form-control" type="color" ref={input => this.colorInput = input} onChange={this.updateColorTheme.bind(this)}/>
+			</div>
 		</div>);
 	}
 }
