bf36f9b5dfc992dabda4a1fcca35a9cdadcfdd05
Paweł Kapalla <pkapalla@xesenix.pl>
2018-03-04 05:47:17 +0100

    LOCALISATION-00 chore(i18n): extract translations

--- scripts/extract.ts
+++ scripts/extract.ts
@@ -1,18 +1,23 @@
-import { mkdirp } from 'mkdirp';
 import { GettextExtractor, HtmlExtractors, JsExtractors } from 'gettext-extractor';
+import { mkdirp } from 'mkdirp';
+import * as path from 'path';
 
+import { getAppConfig, getEnvApp } from '../conf/app/app.config';
+
+const app = getEnvApp();
+const config = getAppConfig(app);
 const extractor = new GettextExtractor();
 
 // handle getText('Segment to extract')
 extractor
 	.createJsParser([
-		JsExtractors.callExpression('getText', {
+		JsExtractors.callExpression('__', {
 			arguments: {
 				text: 0,
 				context: 1,
 			},
 		}),
-		JsExtractors.callExpression('getPlural', {
+		JsExtractors.callExpression('_p', {
 			arguments: {
 				text: 1,
 				textPlural: 2,
@@ -20,14 +25,14 @@ extractor
 			},
 		}),
 	])
-	.parseFilesGlob('./src/**/*.@(ts|js|tsx|jsx)');
+	.parseFilesGlob(path.join(config.rootDir, './**/*.@(ts|js|tsx|jsx)'));
 
 // handle <p translate>Segment to extract</p>
 extractor
 	.createHtmlParser([
 		HtmlExtractors.elementContent('translate, [translate]'),
 	])
-	.parseFilesGlob('./src/**/*.html');
+	.parseFilesGlob(path.join(config.rootDir, './**/*.html'));
 
 mkdirp.sync('./src/locales/');
 extractor.savePotFile('./src/locales/messages.pot');
