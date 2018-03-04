b336d2cd4844809d427cf79783b8819995571ae8
Paweł Kapalla <pkapalla@xesenix.pl>
2018-03-04 22:38:25 +0100

    KARMA-04 fix(coverage): don't include barrel files

--- src/main.test.js
+++ src/main.test.js
@@ -3,7 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const excludeRegexp = /\.\/main\./;
+const excludeRegexp = /\..*\/(main|index)\./;
 /**
  * We need to load all test files to be included in karma. And all others to generate test coverage.
  * https://github.com/webpack-contrib/karma-webpack#alternative-usage
