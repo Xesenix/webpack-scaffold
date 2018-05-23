98e0f68e6dbcdfc811ea7a852d9874367922cf46
Paweł Kapalla <pkapalla@xesenix.pl>
2018-05-24 00:38:23 +0200

    WPS-23 chore(package): update dependencies - xes-webpack-core

--- karma.conf.js
+++ karma.conf.js
@@ -3,5 +3,5 @@ const { karma } = require('xes-webpack-core');
 const webpack = require('./webpack.config.js')();
 
 module.exports = function(config) {
-	karma.configure(config, webpack);
+	config.set(karma.configure(webpack));
 }
